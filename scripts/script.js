fcc92acf895bbd72583925f4050044ba094375d2
Roman Ponomarjov <romanponomarjov@gmail.com>
2022-06-24 15:58:55 +0300

    stage

--- scripts/script.js
+++ scripts/script.js
@@ -5,12 +5,20 @@ const introduction = document.querySelector('.introduction');
 // Link "services"
 const servicesLink = document.querySelector('.main-nav__services-list');
 // Container with list of services
-const servicesContainer = document.querySelector('.services-list')
+const servicesContainer = document.querySelector('.services-list');
 // All other links in navigation
-const mainNavLinks = document.querySelectorAll('.main-nav__link');
+// const mainNavLinks = document.querySelectorAll('.main-nav__link');
+
+// About us link
+const aboutUs = document.querySelector('.about-us');
 
 // Language selector
 const langSelector = document.querySelector('.lang-selector__list');
+
+// Portfolio photos
+const portfolioPhotos = document.querySelectorAll('.portfolio__item')
+
+/*--- Controls for introduction section ---*/
 
 // Function that hides introduction section
 function hideIntro () {
@@ -28,11 +36,26 @@ function restoreIntro () {
 servicesLink.addEventListener('click', hideIntro);
 
 // Event listener for restoring the intro section
-mainNavLinks.forEach(element => {
-    element.addEventListener('mouseenter', restoreIntro)
-});
+// mainNavLinks.forEach(element => {
+//     element.addEventListener('mouseenter', restoreIntro)
+// });
+
+aboutUs.addEventListener('mouseenter', restoreIntro);
+
+/*--- Language selector controls ---*/
 
 // Function to collapse language selector
 langSelector.addEventListener('click', () => {
     langSelector.classList.toggle('collapsed');
 })
+
+// Fulscreen photo controls
+portfolioPhotos.forEach(element => {
+    element.addEventListener('click', makeFullscreen);
+});
+
+function makeFullscreen (element) {
+    console.log('click');
+    element.currentTarget.classList.toggle('fullscreen-photo');
+    console.log(element.currentTarget)
+}
